7a7acba58fb4677f0b7c7d340fcea1743c2a0efe
Egbetimmy <egbetiimmy@gmail.com>
2023-03-04 18:57:36 +0100

    porosity viz

--- app2.py
+++ app2.py
@@ -1,12 +1,6 @@
-# petrophysics
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-import plotly.express as px
-from class1 import Volumetrics
-from class2 import Petrophysics
-from class3 import Anisotropy
 
 st.set_page_config(layout="wide")
 
@@ -21,72 +15,75 @@ def home(uploaded_file):
 
 # TODO
 def data_summary():
-    area = input()
-    fvf = input()
-    test = Volumetrics(df, area, fvf)
-    x = test.bulk_volume()
     st.header('Statistics of Dataframe')
     st.write(df.describe())
-    st.write(x)
 
 
 # TODO
 def data_header():
-    test = Anisotropy(df)
-    x = test.thomsen_delta()
-    y = test.correction()
     st.header('Header of Dataframe')
     st.write(df.head())
-    st.write(x)
-    st.write(y)
-    z = test.plot_data()
-    st.pyplot(z)
 
 
 # TODO
 def displayplot():
-    test = Volumetrics(df)
-    x = test.bulk_volume()
-    st.header('Header of Dataframe')
-    st.write(df.head())
-    st.write(x)
-
-
-# TODO
-def displayplot():
+    st.header('Plot of Data')
+    """
     test = Anisotropy(df)
-    x = test.thomsen_delta()
-    y = test.correction()
-    st.header('Header of Dataframe')
-    st.write(df.head())
-    st.write(x)
-    st.write(y)
+
     z = test.plot_data()
-    st.pyplot(z)
+    """
+    fig, axes = plt.subplots(figsize=(10, 10))
+    curve_names = ['Thomsen_delta', 'Anisotropy_correction', "Vint_cks(m/s)", "Vint_seismic(m/s)"]
 
+    # TODO: try to make it just two plots. Try it out and see how it works.
 
-# TODO
-def interactive_plot():
-    col1, col2 = st.columns(2)
+    ax1 = plt.subplot2grid((1, 3), (0, 0), rowspan=1, colspan=1)
+    ax2 = plt.subplot2grid((1, 3), (0, 1), rowspan=1, colspan=1)
+    ax3 = plt.subplot2grid((1, 3), (0, 2), rowspan=1, colspan=1)
+    ax4 = ax3.twiny()
 
-    x_axis_val = col1.selectbox('Select the X-axis', options=df.columns)
-    y_axis_val = col2.selectbox('Select the Y-axis', options=df.columns)
+    ax1.plot('Thomsen_delta', 'Depth', data=df, color="green", lw=0.5)
+    ax1.set_xlim(df.Thomsen_delta.min(), df.Thomsen_delta.max())
 
-    plot = px.scatter(df, x=x_axis_val, y=y_axis_val)
-    st.plotly_chart(plot, use_container_width=True)
+    ax2.plot('Anisotropy_correction', 'Depth', data=df, color="red", lw=0.5)
+    ax2.set_xlim(df.Anisotropy_correction.min(), df.Anisotropy_correction.max())
+
+    ax3.plot('Vint_cks(m/s)', 'Depth', data=df, color="blue", lw=0.5)
+    ax3.set_xlim(df['Vint_cks(m/s)'].min(), df['Vint_cks(m/s)'].max())
+
+    ax4.plot('Vint_seismic(m/s)', 'Depth', data=df, color="red", lw=0.5)
+    ax4.set_xlim(df['Vint_cks(m/s)'].min(), df['Vint_cks(m/s)'].max())
+
+    for i, ax in enumerate(fig.axes):
+        ax.set_ylim(2154, 1350)
+
+        ax.xaxis.set_ticks_position("top")
+        ax.xaxis.set_label_position("top")
+        ax.set_xlabel(curve_names[i])
+
+        if i == 3:
+            ax.spines["top"].set_position(("axes", 1.08))
+        else:
+            ax.grid()
+
+    for ax in [ax2, ax3, ax4]:
+        plt.setp(ax.get_yticklabels(), visible=False)
+
+    st.pyplot(fig)
 
 
 # Add a title and intro text
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
+st.title('Anisotropy Data Visualization')
+st.text('This is a web app to allow Visualization of Anisotropy Data')
 
 # Sidebar setup
 st.sidebar.title('Sidebar')
-upload_file = st.sidebar.file_uploader('Upload a file containing earthquake data')
+upload_file = st.sidebar.file_uploader('Upload a file containing Anisotropy data')
 # Sidebar navigation
 st.sidebar.title('Navigation')
 options = st.sidebar.radio('Select what you want to display:',
-                           ['Home', 'Data Summary', 'Data Header', 'Data Plot', 'Fancy Plots'])
+                           ['Home', 'Data Summary', 'Data Header', 'Data Visualization'])
 
 # Check if file has been uploaded
 if upload_file is not None:
@@ -99,7 +96,5 @@ elif options == 'Data Summary':
     data_summary()
 elif options == 'Data Header':
     data_header()
-elif options == 'Data Plot':
+elif options == 'Data Visualization':
     displayplot()
-elif options == 'Interactive Plots':
-    interactive_plot()
